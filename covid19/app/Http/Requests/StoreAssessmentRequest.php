5240e7262ce7cdb502c2eee577e7a2271c2fc810
SadmaFaahiim <faahimsadman95@gmail.com>
2026-09-09 17:01:35 +0600

    fix: resolve 10 low-severity code quality issues (L-01 through L-10)
    - Add explicit \ and \ properties to Covid model (L-01, L-07)
    - Modernize route syntax from string to array controller references (L-02)
    - Add Form Request validation classes for all form submissions (L-03)
    - Bump PHP requirement to ^7.4 and replace abandoned fzaninotto/faker
      with fakerphp/faker in composer.json (L-04)
    - Replace default Laravel README with project-specific documentation (L-05)
    - Add CovidSeeder with sample assessment records for demo data (L-06)
    - Update .env.example with APP_KEY placeholder and correct DB name (L-08)
    - Fix 'Syste m' typo in layout footer to 'System' (L-09)
    - Rename resources/views/Layouts to layouts for case consistency (L-10)

---
+++ covid19/app/Http/Requests/StoreAssessmentRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAssessmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'     => 'required|string|max:255',
+            'age'      => 'required|integer|min:1|max:120',
+            'sex'      => 'required|in:male,female',
+            'bodytemp' => 'required|numeric|min:98.1|max:104.0',
+        ];
+    }
+}
--- covid19/resources/views/layouts/layout.blade.php
+++ covid19/resources/views/layouts/layout.blade.php
@@ -17,7 +17,7 @@
        @yield('content')
 
         <div class="footer">
-        <p>This COVID 19 Self Assessment Syste m is only for software development purpose and may not yield actual result. Any information given by users of this system will not be disclosed or store to anywhere.</p>
+        <p>This COVID 19 Self Assessment System is only for software development purpose and may not yield actual result. Any information given by users of this system will not be disclosed or store to anywhere.</p>
       </div>
     
     </body>
